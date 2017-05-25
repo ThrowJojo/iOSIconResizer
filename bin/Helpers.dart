6a33e94f458e4cf84896451f98a06044f1dbe2e7
Jordan Holland <thisrememo@gmail.com>
2017-05-25 11:27:26 +1000

    Started building out Helper class, adding tests.

---
+++ bin/Helpers.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+import 'package:yaml/yaml.dart';
+import 'package:image/image.dart';
+import 'package:path/path.dart' show dirname;
+
+class IconSize {
+  String name;
+  int dimension;
+  IconSize(this.name, this.dimension);
+}
+
+class Helpers {
+
+  // Loads icon sizes from config.yaml file
+  static List<IconSize> loadIconSizes(String configPath) {
+    String configData = new File(configPath).readAsStringSync();
+    var document = loadYaml(configData);
+    var specs = document['sizes'];
+    List<IconSize> iconSizes = [];
+    for (var spec in specs) {
+      IconSize iconSize = new IconSize(spec['name'], spec['dimension']);
+      iconSizes.add(iconSize);
+    }
+    return iconSizes;
+  }
+
+  // Loads an image from the @{filePath}, returns nil if there is an issue
+  static Image loadImage(String filePath) {
+    try {
+      return decodeImage(new File(filePath).readAsBytesSync());
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // Resizes the provided @{image} and then saves it
+  static resizeImageAndSave(Image image, IconSize iconSize) {
+    Image resizedImage = copyResize(image, iconSize.dimension);
+    new File("${iconSize.name}.png")..writeAsBytesSync(encodePng(resizedImage));
+  }
+
+  // Copies a file to @{newFilePath}
+  static copyFile(String filePath, String newFilePath) {
+    try {
+     File file = new File(filePath);
+     file.copySync(newFilePath);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+}
--- test/iOSIconResizer_test.dart
+++ test/iOSIconResizer_test.dart
@@ -2,10 +2,47 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:iOSIconResizer/iOSIconResizer.dart';
+import 'package:image/image.dart';
 import 'package:test/test.dart';
+import '../bin/Helpers.dart';
+import 'dart:io';
 
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
   });
+
+  test('loadImage', () {
+    Image image = Helpers.loadImage("icon.png");
+    expect(image == null, false);
+    Image image2 = Helpers.loadImage("icon2.png");
+    expect(image2, null);
+  });
+
+  test('loadIconSizes', () {
+    List<IconSize> iconSizes = Helpers.loadIconSizes("../bin/config.yaml");
+    expect(iconSizes.length > 0, true);
+  });
+
+  test('resizeImageAndSave', () {
+    Image image = Helpers.loadImage("icon.png");
+    expect(image == null, false);
+    List<IconSize> iconSizes = Helpers.loadIconSizes("../bin/config.yaml");
+    expect(iconSizes.length > 0, true);
+    Helpers.resizeImageAndSave(image, iconSizes[0]);
+    Image resizedImage = Helpers.loadImage("${iconSizes[0].name}.png");
+    expect(resizedImage == null, false);
+  });
+
+  test('exists', () async {
+    bool exists = await new File("file_that_doesnt_exist.png").exists();
+    expect(exists, false);
+  });
+
+  test('copyFile', () async {
+    Helpers.copyFile("icon.png", "copied_icon.png");
+    bool exists = await new File("copied_icon.png").exists();
+    expect(exists, true);
+  });
+
 }
