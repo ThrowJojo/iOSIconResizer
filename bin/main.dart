fe12bbc14d0de1f63ccc5fc1aefde2c4006b3c38
Jordan Holland <thisrememo@gmail.com>
2017-05-31 13:06:37 +1000

    Started adding argument parser.

--- bin/main.dart
+++ bin/main.dart
@@ -4,20 +4,28 @@
 import 'package:iOSIconResizer/iOSIconResizer.dart' as iOSIconResizer;
 import 'Helpers.dart';
 import 'package:image/image.dart';
-
+import 'package:args/args.dart';
 
 main(List<String> arguments) {
-  if (arguments.length > 0) {
-    Image loadedImage = Helpers.loadImage(arguments[0]);
+
+  var parser = new ArgParser();
+  parser.addOption('folder');
+  parser.addOption('icon');
+  var results = parser.parse(arguments);
+  String icon = results['icon'];
+
+  if (icon != null) {
+    Image loadedImage = Helpers.loadImage(icon);
     if (loadedImage != null) {
       String path = Helpers.getPath();
       List<IconSize> iconSizes = Helpers.loadIconSizes("${path}/config.yaml");
       Helpers.resizeImageToSizes(loadedImage, iconSizes);
       Helpers.copyFile("${path}/Contents.json", "Contents.json");
     } else {
-      print("There was an issue loading the specified image file, make sure it exists.");
+      print("There was an issue loading the specified icon file, make sure it exists.");
     }
   } else {
-    print("No image specified.");
+    print("No icon specified.");
   }
+
 }
